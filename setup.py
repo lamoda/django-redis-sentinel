c032d78cbc045fff2a89d74049c860c3f99450d0
Rafał Furmański <r.furmanski@gmail.com>
2020-11-16 16:02:59 +0100

    Allow to use newer django-redis

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
-        "django-redis>=4.7.0,<=4.10.0",
+        "django-redis>=4.7.0",
     ],
     zip_safe=False,
     include_package_data=True,
